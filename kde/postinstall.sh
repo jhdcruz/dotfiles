b03235032693da8b909fffe13ad26113652824a2
Joshua Hero Dela Cruz <jhdcruz00@gmail.com>
2022-05-11 09:56:26 +0800

    Delete postinstall.sh

--- kde/postinstall.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -Sy ksshaskpass --noconfirm
-
-# Solve VSCode's ssh-askpass error
-# https://forum.manjaro.org/t/vscode-git-ssh-askpass-exec-usr-lib-ssh-ssh-askpass-no-such-file-or-directory/78787
-sudo ln /usr/bin/ksshaskpass /usr/lib/ssh/ssh-askpass
