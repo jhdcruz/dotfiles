12929ac3e398786fd677af741ddabeeebd1ca92c
Joshua Hero <jhdcruz@proton.me>
2023-06-26 02:28:02 +0800

    update dotfiles

--- .zshrc
+++ .zshrc
@@ -1,6 +1,3 @@
-# REQUIRES: https://github.com/romkatv/powerlevel10k#installation
-#           git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions --depth 1
-
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -8,105 +5,63 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
 
-# Path to your oh-my-zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
+setopt autocd nomatch
+unsetopt beep extendedglob notify
 
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+bindkey -e
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+bindkey "\e[A" history-search-backward
+bindkey "\e[B" history-search-forward
 
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
 
-# Uncomment the following line to use hyphen-insensitive completion.
-# Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
+# End of lines configured by zsh-newuser-install
 
-# Uncomment one of the following lines to change the auto-update behavior
-# zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
-# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
 
-# Uncomment the following line to change how often to auto-update (in days).
-# zstyle ':omz:update' frequency 13
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
 
-# Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS="true"
+setopt promptsubst
 
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
 
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
+zinit light-mode silent wait lucid for \
+    zsh-users/zsh-autosuggestions \
+    zdharma-continuum/fast-syntax-highlighting
 
-# Uncomment the following line to enable command auto-correction.
-ENABLE_CORRECTION="true"
+zinit snippet 'https://github.com/robbyrussell/oh-my-zsh/raw/master/plugins/git/git.plugin.zsh'
 
-# Uncomment the following line to display red dots whilst waiting for completion.
-# You can also set it to another string to have that shown instead of the default red dots.
-# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
-# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
-# COMPLETION_WAITING_DOTS="true"
+zinit ice depth"1"
+zinit light romkatv/powerlevel10k
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+# END zinit
 
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
+source ~/.rc/aliases.rc
 
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(zsh-autosuggestions dnf git nvm)
-
-NVM_LAZY=1
-
-source $ZSH/oh-my-zsh.sh
-
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-. ~/.bash_aliases
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+eval "$(rbenv init - zsh)"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,97 +2,317 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath
+        {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath
         }
-      )
+    )
 end
 
 vim.opt.rtp:prepend(lazypath)
 
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Set map leader
 vim.g.mapleader = " "
+vim.opt.termguicolors = true
 
 -- PLUGINS
 
 require("lazy").setup({
     "folke/which-key.nvim",
-    "folke/neodev.nvim", 
+    "folke/neodev.nvim",
     "nvim-lua/plenary.nvim",
+    "wakatime/vim-wakatime", -- will prompt for api key on first run
 
     -- Themes
     "nvim-tree/nvim-web-devicons",
     {
-      "Mofiqul/adwaita.nvim",
-      lazy = false,
-      priority = 1000,
+        "Mofiqul/adwaita.nvim",
+        lazy = false,
+        priority = 1000,
 
-      -- configure and set on startup
-      config = function()
-          vim.g.adwaita_disable_cursorline = true -- to disable cursorline
-          vim.cmd('colorscheme adwaita')
-      end
+        -- configure and set on startup
+        config = function()
+            vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+            vim.cmd('colorscheme adwaita')
+        end
     },
-
     {
-      "nvim-lualine/lualine.nvim",
-      requires = {
-          "Mofiqul/lualine-adwaita.nvim",
-      },
-      config = function()
-          require("lualine").setup {
-              options = {
-                  theme = "adwaita",
-              },
-          }
-      end
+        "nvim-lualine/lualine.nvim",
+        requires = {
+            "Mofiqul/lualine-adwaita.nvim",
+        },
+        config = function()
+            require("lualine").setup {
+                options = {
+                    theme = "adwaita",
+                },
+            }
+        end
     },
 
     -- Code
     {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate"
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "antosha417/nvim-lsp-file-operations",
+        },
+        config = function()
+            require("nvim-tree").setup {
+                disable_netrw = true,
+                hijack_netrw = true,
+                open_on_tab = false,
+                hijack_cursor = false,
+                update_cwd = false,
+                update_focused_file = {
+                    enable = true,
+                    update_cwd = false,
+                    ignore_list = {}
+                },
+                system_open = {
+                    cmd = nil,
+                    args = {}
+                },
+                view = {
+                    width = 30,
+                    side = "left",
+                    mappings = {
+                        custom_only = false,
+                        list = {}
+                    }
+                },
+                filters = {
+                    custom = {
+                        "^.git$",
+                        "^node_modules$",
+                        "^.idea$",
+                    }
+                }
+            }
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end
+    },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup()
+        end
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = {
+            "nvim-lua/popup.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        requires = {
+            "nvim-telescope/telescope-fzf-native.nvim",
+        },
+        config = function()
+            require("telescope").setup {}
+        end
     },
 
+    -- LSP, Linters, Formatters
     {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-          require("gitsigns").setup()
-      end
+        "hrsh7th/nvim-cmp",
+        requires = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/vim-vsnip",
+            "hrsh7th/cmp-calc",
+            "hrsh7th/cmp-emoji",
+            "hrsh7th/cmp-look",
+            "hrsh7th/cmp-omni",
+            "hrsh7th/cmp-treesitter",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-ultisnips",
+            "hrsh7th/cmp-words",
+        },
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end
+                },
+                mapping = {
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.close(),
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ['<Tab>'] = function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif vim.fn["vsnip#available"](1) == 1 then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true), '')
+                        else
+                            fallback()
+                        end
+                    end,
+                    ['<S-Tab>'] = function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>(vsnip-jump-prev)', true, true, true),
+                                '')
+                        else
+                            fallback()
+                        end
+                    end,
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'vsnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'nvim_lua' },
+                    { name = 'calc' },
+                    { name = 'emoji' },
+                    { name = 'look' },
+                    { name = 'treesitter' },
+                    { name = 'cmdline' },
+                    { name = 'ultisnips' },
+                    { name = 'words' },
+                }
+            }
+        end
     },
-
     {
-      "windwp/nvim-autopairs",
-      config = function()
-          require("nvim-autopairs").setup()
-      end
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+        config = function()
+            require("mason").setup()
+        end
     },
-
     {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
+        "williamboman/mason-lspconfig.nvim",
+        requires = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers {}
+        end
     },
-
     {
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.2",
-      dependencies = {
-          "nvim-lua/popup.nvim",
-          "nvim-lua/plenary.nvim",
-      },
-      requires = {
-          "nvim-telescope/telescope-fzf-native.nvim",
-      },
-      config = function()
-          require("telescope").setup {}
-      end
-    },
+        "neovim/nvim-lspconfig",
+        requires = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+    }
 })
 
 -- MAPPINGS
+
+-- nvim-tree
+local api = require("nvim-tree.api")
+
+local function edit_or_open()
+    local node = api.tree.get_node_under_cursor()
+
+    if node.nodes ~= nil then
+        -- expand or collapse folder
+        api.node.open.edit()
+    else
+        -- open file
+        api.node.open.edit()
+    end
+end
+
+-- open as vsplit on current node
+local function vsplit_preview()
+    local node = api.tree.get_node_under_cursor()
+
+    if node.nodes ~= nil then
+        -- expand or collapse folder
+        api.node.open.edit()
+    else
+        -- open file as vsplit
+        api.node.open.vertical()
+    end
+
+    -- Finally refocus on tree if it was lost
+    api.tree.focus()
+end
+
+vim.keymap.set('n', '<A-1>', ':NvimTreeToggle<CR>', { silent = true, noremap = true })
+-- nvim-tree mappings defined in local functions above
+vim.keymap.set("n", "l", edit_or_open)
+vim.keymap.set("n", "L", vsplit_preview)
+vim.keymap.set("n", "h", api.tree.close)
+vim.keymap.set("n", "H", api.tree.collapse_all)
+
 -- Telescope mappings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- LSP mappings
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
