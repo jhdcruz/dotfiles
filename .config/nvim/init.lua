05de4192f929d91d5623a6d1f596e8cdc261427b
Joshua Hero <jhdcruz@proton.me>
2023-07-09 20:55:16 +0800

    complete init.vim command migration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,6 +30,20 @@ vim.opt.termguicolors = true
 -- display settings
 vim.opt.wrap = false
 vim.opt.number = true
+vim.opt.textwidth = 0
+vim.opt.cmdheight = 0
+vim.opt.expandtab = true
+vim.opt.shell = "/bin/zsh"
 
--- PLUGINS
+-- split panels
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- split panels using alt + hjkl
+vim.api.nvim_set_keymap("n", "<M-h>", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<M-j>", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<M-k>", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<M-l>", "<C-w>l", { noremap = true })
+
+-- Lazy plugins setup
 require("lazy").setup(Plugins)
